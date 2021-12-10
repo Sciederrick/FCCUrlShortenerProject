f69571eb49467f17514a8ac16d2e0e3d9369e6f8
derrick <derrickmbarani@gmail.com>
2021-12-11 01:08:55 +0300

    validation fix

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.post('/api/shorturl', (req, res) => {
   let original_url = req.body.url;
   try {
     original_url = new URL(original_url);
+    if (!((original_url.protocol === 'http:' || original_url.protocol === 'https:') && 
+        original_url.host.split('.')[0] === 'www' && 
+        original_url.host.split('.').pop() === 'com')) {
+          throw { code: 'ERR_INVALID_URL' };
+        }
     dns.lookup(original_url.host, (err, _) => {
       if (err) {
         throw null;
